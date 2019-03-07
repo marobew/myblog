2baae8b8e333ed6063758ca549594cfbf4d5595b
marobew <boramm@icloud.com>
2019-03-07 13:33:52 +0900

    heroku upload

--- project3/settings.py
+++ project3/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'v-__w4f7vn(g_p&v$yy60cu#5exg6k
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
